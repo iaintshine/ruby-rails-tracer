0899f1a6ff7793ad9170d6563ed1abe6f9a36772
iaintshine <bodziomista@gmail.com>
2017-08-09 22:31:11 +0200

    Introduce auto-instrumentation for ActiveRecord

---
+++ lib/rails/active_record/tracer.rb
@@ -0,0 +1,48 @@
+module ActiveRecord
+  module Tracer
+    DEFAULT_OPERATION_NAME = "sql.query".freeze
+
+    class << self
+      def instrument(tracer: OpenTracing.global_tracer, active_span: nil)
+        ::ActiveSupport::Notifications.subscribe('sql.active_record') do |*args|
+          ActiveRecord::Tracer.sql(tracer: tracer, active_span: active_span, args: args)
+        end
+      end
+
+      def sql(tracer: OpenTracing.global_tracer, active_span: nil, args:)
+        _, start, finish, _, payload = *args
+
+        span = start_span(payload.fetch(:name),
+                          tracer: tracer,
+                          active_span: active_span,
+                          start_time: start,
+                          sql: payload.fetch(:sql),
+                          cached: payload.fetch(:cached, false),
+                          connection_id: payload.fetch(:connection_id))
+
+        span.finish(end_time: finish)
+      end
+
+
+      def start_span(operation_name, tracer: OpenTracing.global_tracer, active_span: nil, start_time: Time.now, **fields)
+        connection_config = ::ActiveRecord::Base.connection_config
+
+        span = tracer.start_span(operation_name || DEFAULT_OPERATION_NAME,
+                                 child_of: active_span.respond_to?(:call) ? active_span.call : active_span,
+                                 start_time: start_time,
+                                 tags: {
+                                  'component' => 'ActiveRecord',
+                                  'span.kind' => 'client',
+                                  'db.user' => connection_config.fetch(:username),
+                                  'db.instance' => connection_config.fetch(:database),
+                                  'db.vendor' => connection_config.fetch(:adapter),
+                                  'db.connection_id' => fields.fetch(:connection_id, 'unknown'),
+                                  'db.cached' => fields.fetch(:cached, false),
+                                  'db.statement' => fields.fetch(:sql),
+                                  'db.type' => 'sql'
+                                 })
+        span
+      end
+    end
+  end
+end
--- lib/rails/tracer.rb
+++ lib/rails/tracer.rb
@@ -1,1 +1,2 @@
 require_relative "rack/tracer"
+require_relative "active_record/tracer"
