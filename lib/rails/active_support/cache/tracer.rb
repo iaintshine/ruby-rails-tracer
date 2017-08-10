3c8b053314302b549a0c0a2a4fe41a6b1f1d3f0f
iaintshine <bodziomista@gmail.com>
2017-08-10 20:40:30 +0200

    Introduce auto-instrumentation for ActiveSupport::Cache

---
+++ lib/rails/active_support/cache/tracer.rb
@@ -0,0 +1,51 @@
+module ActiveSupport
+  module Cache
+    module Tracer
+      class << self
+        # TODO: In Rails 4.0+ it's possible to  subscribe to start events
+        def instrument(tracer: OpenTracing.global_tracer, active_span: nil)
+          events = %w(read write generate delete clear)
+          events.each do |event|
+            ActiveSupport::Notifications.subscribe("cache_#{event}.active_support") do |*args|
+              ActiveSupport::Cache::Tracer.instrument_event(tracer: OpenTracing.global_tracer,
+                                                            active_span: -> { OpenTracing.global_tracer.active_span },
+                                                            event: event,
+                                                            args: args)
+            end
+          end
+        end
+
+        def instrument_event(tracer: OpenTracing.global_tracer, active_span: nil, event:, args:)
+          _, start, finish, _, payload = *args
+
+          span = start_span("cache.#{event}",
+                            event: event,
+                            tracer: tracer,
+                            active_span: active_span,
+                            start_time: start,
+                            **payload)
+
+          span.finish(end_time: finish)
+        end
+
+        def start_span(operation_name, tracer: OpenTracing.global_tracer, active_span: nil, start_time: Time.now, event:, **fields)
+          puts fields
+          span = tracer.start_span(operation_name,
+                                   child_of: active_span.respond_to?(:call) ? active_span.call : active_span,
+                                   start_time: start_time,
+                                   tags: {
+                                    'component' => 'ActiveSupport::Cache',
+                                    'span.kind' => 'client',
+                                    'cache.key' => fields.fetch(:key, 'unknown')
+                                   })
+
+          if event == 'read'
+            span.set_tag('cache.hit', fields.fetch(:hit, false))
+          end
+
+          span
+        end
+      end
+    end
+  end
+end
