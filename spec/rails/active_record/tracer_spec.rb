632998f04d940579a45f65eeec236d46ddf7c2cf
iaintshine <bodziomista@gmail.com>
2017-08-16 19:58:05 +0200

    Add active span propagation tets for ActiveRecord::Tracer

--- spec/rails/active_record/tracer_spec.rb
+++ spec/rails/active_record/tracer_spec.rb
@@ -3,6 +3,29 @@ require "spec_helper"
 RSpec.describe ActiveRecord::Tracer do
   let(:tracer) { Test::Tracer.new }
 
+  describe "active span propagation" do
+    let(:root_span) { tracer.start_span("root") }
+
+    before do
+      @active_record_tracer = ActiveRecord::Tracer.instrument(tracer: tracer, active_span: -> { root_span })
+      Article.first
+    end
+
+    after do
+      ActiveSupport::Notifications.unsubscribe(@active_record_tracer)
+    end
+
+    it "creates the new span with active span trace_id" do
+      cache_span = tracer.finished_spans.last
+      expect(cache_span.context.trace_id).to eq(root_span.context.trace_id)
+    end
+
+    it "creates the new span with active span as a parent" do
+      cache_span = tracer.finished_spans.last
+      expect(cache_span.context.parent_span_id).to eq(root_span.context.span_id)
+    end
+  end
+
   describe "auto-instrumentation" do
     before do
       @active_record_tracer = ActiveRecord::Tracer.instrument(tracer: tracer)
@@ -19,9 +42,6 @@ RSpec.describe ActiveRecord::Tracer do
 
     it "sets operation_name to event's name" do
       expect(tracer.finished_spans.first.operation_name).to eq("Article Load")
-    end
-
-    it "propagates context" do
     end
 
     it "sets standard OT tags" do
