87bbd05504697f8b743850de9880cf96a0ff5738
iaintshine <bodziomista@gmail.com>
2017-09-05 00:40:10 +0200

    Fix migration class to be compatible with Rails versions < 5

--- spec/test_app/db/migrate/20170815215418_create_articles.rb
+++ spec/test_app/db/migrate/20170815215418_create_articles.rb
@@ -1,4 +1,10 @@
-class CreateArticles < ActiveRecord::Migration[5.1]
+superclass = if Gem::Version.new(Rails.version) >= Gem::Version.new(5)
+              ActiveRecord::Migration[5.0]
+            else
+              ActiveRecord::Migration
+            end
+
+class CreateArticles < superclass
   def change
     create_table :articles do |t|
       t.string :title
