ea51a9143ac9f5e6e5f320cc2a56f526bb5f7975
iaintshine <bodziomista@gmail.com>
2017-09-07 00:53:45 +0200

    Expose a tracer which will auto-instrument all sub modules

--- lib/rails/tracer.rb
+++ lib/rails/tracer.rb
@@ -2,3 +2,20 @@ require "rails/span_helpers"
 require "rails/rack/tracer"
 require "rails/active_record/tracer"
 require "rails/active_support/cache/tracer"
+
+module Rails
+  module Tracer
+    class << self
+      def instrument(tracer: OpenTracing.global_tracer, active_span: nil, dalli: false)
+        Rails::Rack::Tracer.instrument
+        ActiveSupport::Cache::Tracer.instrument(tracer: tracer, active_span: active_span, dalli: dalli)
+        ActiveRecord::Tracer.instrument(tracer: tracer, active_span: active_span)
+      end
+
+      def disable
+        ActiveRecord::Tracer.disable
+        ActiveSupport::Cache::Tracer.disable
+      end
+    end
+  end
+end
